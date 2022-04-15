934547baaad90e38af2f68533212aca6665ba39f
Islem Eddine Bougroura <islam.bougroura@univ-constantine2.dz>
2022-04-15 04:32:00 +0100

    configured settings.py to connect to mysql

--- dzest/settings.py
+++ dzest/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'dzest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dzest',
+        'USER': 'root',
+        'PASSWORD': '200120032012',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
