c32e6573bb30e53d50978adb0e0624545db73cd6
Islem Eddine Bougroura <islam.bougroura@univ-constantine2.dz>
2022-04-28 14:45:17 +0100

    updated models

--- base/models.py
+++ base/models.py
@@ -12,14 +12,6 @@ class Agency(models.Model):
     def __str__(self):
         return self.name
 
-class Client(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return "%s %s" % (self.first_name, self.last_name)
-
 class UserLocation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     state = models.CharField(max_length=50)
@@ -37,14 +29,17 @@ class Offer(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     price = models.IntegerField()
-    transaction_type = models.CharField(max_length=30)
-    property_type  = models.CharField(max_length=30)
-    property_area = models.CharField(max_length=30)
-    negotiability = models.BooleanField()
-    notarial_act = models.BooleanField()
-    electricity = models.BooleanField()
-    gaz = models.BooleanField()
-    internet = models.BooleanField()
+    # transaction_type = models.CharField(max_length=30)
+    # property_type  = models.CharField(max_length=30)
+    # property_area = models.CharField(max_length=30)
+    # negotiability = models.BooleanField()
+    # notarial_act = models.BooleanField()
+    # electricity = models.BooleanField()
+    # gaz = models.BooleanField()
+    # internet = models.BooleanField()
+    
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.title
@@ -68,6 +63,14 @@ class OfferLocation(models.Model):
     def __str__(self):
         return "%s, %s, %s, %s, %s" % (self.state, self.municipal, self.street_adress, self.longtitude, self.latitude)
 
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    followed = models.ManyToManyField(Agency, related_name='followed', blank=True,)
+    favourites = models.ManyToManyField(Offer, related_name='favourites', blank=True,)
+
+    def __str__(self):
+        return "%s %s" % (self.user.first_name, self.user.last_name)
+
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
@@ -77,19 +80,19 @@ class Message(models.Model):
     def __str__(self):
         return "%s: %s, %s" % (self.user, self.text, self.created)
 
-class Follow(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    agency = models.ForeignKey(Agency, on_delete=models.CASCADE)
+# class Follow(models.Model):
+#     client = models.ForeignKey(Client, on_delete=models.CASCADE)
+#     agency = models.ForeignKey(Agency, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return "%s follows %s" % (self.client, self.agency)
+#     def __str__(self):
+#         return "%s follows %s" % (self.client, self.agency)
 
-class Favourite(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+# class Favourite(models.Model):
+#     client = models.ForeignKey(Client, on_delete=models.CASCADE)
+#     offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return "%s likes %s" % (self.client, self.offer)
+#     def __str__(self):
+#         return "%s likes %s" % (self.client, self.offer)
 
 
 
