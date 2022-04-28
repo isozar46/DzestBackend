28fb93badad0ff96cc13f4c6f8725ad397da4ed4
Islem Eddine Bougroura <islam.bougroura@univ-constantine2.dz>
2022-04-28 14:44:50 +0100

    updated admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Client, Agency, Offer
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Agency)
+admin.site.register(Offer)
